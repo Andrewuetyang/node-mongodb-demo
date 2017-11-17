64e95323a9507ae861fa6a05e63e2293b5629e81
Andrewuet <andrewuetyang@gmail.com>
2017-11-17 18:22:27 +0800

    初步完善增删改查

--- controller/test/todo.js
+++ controller/test/todo.js
@@ -6,18 +6,79 @@ class TodoList {
   async add (req, res, next) {
     const idData = await Ids.findOne()
     idData.todo_id++
-    await idData.save()
-    const newTodo = new Todo({
-      id: idData.todo_id,
-      content: req.body.content,
-      time: req.body.time,
-      status: 1
-    })
-    await newTodo.save()
-    res.send({
-      code: 0,
-      message: '保存成功'
-    })
+    try {
+      await idData.save()
+      const newTodo = new Todo({
+        id: idData.todo_id,
+        content: req.body.content,
+        time: req.body.time,
+        status: 1
+      })
+      await newTodo.save()
+      res.send({
+        code: 0,
+        message: '保存成功'
+      })
+    } catch (err) {
+      console.log('新增todo失败', err);
+			res.send({
+				status: 0,
+				message: '新增todo失败'
+			})
+    }
+    
+  }
+  async delete (req, res, next) {
+    const id = req.body.id
+    try {
+      await Todo.findOneAndRemove({id: id})
+      res.send({
+        code: 0,
+        message: '删除成功'
+      })
+    } catch (err) {
+      console.log('查找删除操作失败', err);
+			res.send({
+				status: 0,
+				message: '查找删除操作失败'
+			})
+    }
+  }
+  async update (req, res, next) {
+    const {id, status, content, time} = req.body
+    let updateObj = {}
+    if (status) updateObj.status = status
+    if (content) updateObj.content = content
+    if (time) updateObj.time = time
+    try {
+      await Todo.findOneAndUpdate({id: id}, updateObj)
+      res.send({
+        code: 0,
+        message: '更改成功'
+      })
+    } catch (err) {
+      console.log('更改操作失败', err);
+			res.send({
+				status: 0,
+				message: '更改操作失败'
+			})
+    }
+  }
+  async list (req, res, next) {
+    console.log(111);
+    try {
+      const list = await Todo.find()
+      res.send({
+        code: 0,
+        data: list
+      })
+    } catch (err) {
+      console.log('todo列表获取失败', err);
+			res.send({
+				status: 0,
+				message: 'todo列表获取失败'
+			})
+    }
   }
 }
 
--- routes/test.js
+++ routes/test.js
@@ -4,5 +4,8 @@ import TodoList from '../controller/test/todo'
 const router = express.Router()
 
 router.post('/todo/add', TodoList.add)
+router.post('/todo/delete', TodoList.delete)
+router.post('/todo/update', TodoList.update)
+router.get('/todo/list', TodoList.list)
 
 export default router
